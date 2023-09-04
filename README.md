4afc55ca81785eb463ba5581941188181f66c2fd
shenan.quek <77522687+SQUEK011@users.noreply.github.com>
2023-09-05 00:15:18 +0800

    commit 5/9/23

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# image-gallery-js
- An Image Gallery using Javascript 
+# Image Gallery
+
+The Image Gallery is a web application that allows users to view and filter a collection of images.
+
+## Table of Contents
+
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [How to Use](#how-to-use)
+- [Contributing](#contributing)
+- [Issues](#issues)
+
+## Features
+
+- Display a grid of images with tags or categories.
+- Filter images based on tags or categories.
+- View image details in a modal viewer.
+- Responsive design for various screen sizes.
+
+## Technologies Used
+
+- HTML5 and CSS3
+- JavaScript (ES6+)<!-- Object-Oriented Programming (OOP) principle-->
+- [Bootstrap](https://getbootstrap.com/) (for styling)
+- [Git](https://git-scm.com/) (version control)
+- [TensorArt](https://tensor.art/) (Image Creation)
+
+## How to Use
+
+1. Clone this repository to your local machine:
+
+```bash
+git clone https://github.com/yourusername/image-gallery.git
+cd image-gallery
+```
+2. Open index.html in your web browser.
+3. Use the filter buttons to select specific tags or categories.
+4. Click on an image to view it in a modal with detailed information.
+
+## Contributing
+If you'd like to contribute to this project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix:
+
+```bash
+git checkout -b feature/my-feature
+```
+3. Commit your changes and push them to your forked repository:
+
+```bash
+git commit -m "Add my feature"
+git push origin feature/my-feature
+```
+3. Create a pull request from your forked repository to the original repository.
+4. Wait for the maintainers to review your pull request.
+
+## Issues
+If you encounter any issues or have suggestions for improvements, please [open an issue](https://github.com/squek011/image-gallery-js/issues) on this repository.
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,103 @@
 //Centralized Codes here
+// Get references to important elements
+const filterButtons = document.querySelectorAll('.filter-button');
+const modal = document.getElementById('modal');
+const modalContent = document.createElement('div');
+modalContent.className = 'modal-content';
+modal.appendChild(modalContent);
+
+// Sample image data (you can replace this with your actual data)
+const imageData = [
+    {
+      title: "Image 1",
+      description: "Description for Image 1.",
+      tags: ["ai", "realistic"],
+      url: "../assets/ai-image1.png",
+    },
+    // Add more image objects here...
+    {
+      title: "Image 2",
+      description: "Description for Image 2.",
+      tags: ["ai", "realistic"],
+      url: "../assets/real-image1.png",
+    },
+    {
+      title: "Image 3",
+      description: "Description for Image 3.",
+      tags: ["ai", "items"],
+      url: "../assets/items-image1.png",
+    },
+    {
+      title: "Image 4",
+      description: "Description for Image 4.",
+      tags: ["ai", "cars"],
+      url: "../assets/cars-image1.png",
+    },
+  ];
+
+// Function to populate the image gallery
+function populateGallery(filter) {
+    const gallery = document.querySelector('.row');
+    gallery.innerHTML = '';
+
+    imageData.forEach((image) => {
+        if (filter === 'all' || image.tags.includes(filter)) {
+            const col = document.createElement('div');
+            col.className = 'col-md-4 image-item ' + image.tags.join(' ');
+            col.innerHTML = `
+                <img src="${image.url}" alt="${image.title}">
+                <div class="image-info">
+                    <h3>${image.title}</h3>
+                    <p>${image.description}</p>
+                </div>
+            `;
+            
+            // Add click event to open modal
+            col.addEventListener('click', () => {
+                showModal(image);
+            });
+
+            gallery.appendChild(col);
+        }
+    });
+}
+
+// Function to show the modal with image details
+function showModal(image) {
+    modalContent.innerHTML = `
+    <span class="close">&times;</span>
+    <div class="image-container">
+        <img src="${image.url}" alt="${image.title}" class="modal-image">
+    </div>
+    <div class="image-info">
+        <h3>${image.title}</h3>
+        <p>${image.description}</p>
+    </div>
+    `;
+
+    modal.style.display = 'block';
+
+    // Close the modal when the 'x' is clicked
+    const closeButton = document.querySelector('.close');
+    closeButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    // Close the modal when clicking outside the modal content
+    window.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+}
+
+// Event listeners for filter buttons
+filterButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const filter = button.getAttribute('data-filter');
+        populateGallery(filter);
+    });
+});
+
+// Initial gallery population
+populateGallery('all');
